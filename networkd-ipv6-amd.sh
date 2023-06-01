a4c1742d34a0b5f26e53e11cad18949bebb57683
orcooo <114367118+orcooo@users.noreply.github.com>
2023-06-01 20:56:04 +0800

    Update networkd-ipv6-amd.sh

--- networkd-ipv6-amd.sh
+++ networkd-ipv6-amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /etc/systemd/network
-wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/enp0s3.zip && unzip enp0s3.zip && rm enp0s3.zip
+wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/ens3.zip && unzip ens3.zip && rm ens3.zip
 sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd && wq && apt purge -y --auto-remove ifupdown isc-dhcp-client
