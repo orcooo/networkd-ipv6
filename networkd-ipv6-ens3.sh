e538e9e579d739a9f0dcc4cbce6b0c418538c562
orcooo <114367118+orcooo@users.noreply.github.com>
2023-06-01 22:18:20 +0800

    Update networkd-ipv6-ens3.sh

--- networkd-ipv6-ens3.sh
+++ networkd-ipv6-ens3.sh
@@ -2,4 +2,4 @@
 
 cd /etc/systemd/network
 wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/ens3.zip && unzip ens3.zip && rm ens3.zip
-sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd && wq && apt purge -y --auto-remove ifupdown isc-dhcp-client
+sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd
