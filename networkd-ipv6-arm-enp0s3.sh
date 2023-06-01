7a85281e2278ce0723c0901380ddad07ab62cdd3
orcooo <114367118+orcooo@users.noreply.github.com>
2023-06-01 21:43:20 +0800

    Update networkd-ipv6-arm-enp0s3.sh

--- networkd-ipv6-arm-enp0s3.sh
+++ networkd-ipv6-arm-enp0s3.sh
@@ -2,4 +2,4 @@
 
 cd /etc/systemd/network
 wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/enp0s3.zip && unzip enp0s3.zip && rm enp0s3.zip
-sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd && wq && apt purge -y --auto-remove ifupdown isc-dhcp-client
+sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd && apt purge -y --auto-remove ifupdown isc-dhcp-client
