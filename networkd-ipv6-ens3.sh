7bc2c4832af2e8f8c89e489ff704dc903cb6fd73
orcooo <114367118+orcooo@users.noreply.github.com>
2023-06-01 22:49:04 +0800

    Update networkd-ipv6-ens3.sh

--- networkd-ipv6-ens3.sh
+++ networkd-ipv6-ens3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /etc/systemd/network
-wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/ens3.zip && unzip ens3.zip && rm ens3.zip
-sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd
+wget -N https://github.com/orcooo/networkd-ipv6/releases/download/1.0/ens3.zip && unzip ens3.zip && rm ens3.zip && sudo systemctl enable systemd-networkd && sudo systemctl restart systemd-networkd && sudo systemctl status systemd-networkd
